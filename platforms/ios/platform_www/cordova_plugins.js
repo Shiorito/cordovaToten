66f9c1c42ee2495615e51d900085189a8d593746
村社光誠 <kosei@MuraMac.local>
2020-07-05 14:38:54 +0900

    first commit

---
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,33 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-splashscreen.SplashScreen",
+      "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+      "pluginId": "cordova-plugin-splashscreen",
+      "clobbers": [
+        "navigator.splashscreen"
+      ]
+    },
+    {
+      "id": "cordova-plugin-wkwebview-engine.ios-wkwebview-exec",
+      "file": "plugins/cordova-plugin-wkwebview-engine/src/www/ios/ios-wkwebview-exec.js",
+      "pluginId": "cordova-plugin-wkwebview-engine",
+      "clobbers": [
+        "cordova.exec"
+      ]
+    },
+    {
+      "id": "cordova-plugin-wkwebview-engine.ios-wkwebview",
+      "file": "plugins/cordova-plugin-wkwebview-engine/src/www/ios/ios-wkwebview.js",
+      "pluginId": "cordova-plugin-wkwebview-engine",
+      "clobbers": [
+        "window.WkWebView"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-splashscreen": "5.0.4",
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-wkwebview-engine": "1.2.1"
+  };
+});
